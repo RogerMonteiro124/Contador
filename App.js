0b2ed20bc74a85362f69e4b73af4c62f92694235
rogermonteiro124 <roger-15monteiro@hotmail.com>
2020-05-17 21:42:33 -0400

    Jogo da velha

--- App.js
+++ App.js
@@ -1,55 +1,314 @@
 /**
- *Projeto simples que cria um contador incremental com opção de 
- *zerar o contador e iniciar novamente.
+ *Projeto simples de um jogo da velha em React Native.
 **/
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
+  Button,
+  Alert
 } from 'react-native';
 
 const App: () => React$Node = () => {
-  const [count, setCount] = useState(0);
-  const Increment =()=>{
-    setCount((count+1)*2);
+
+  const [ums,setUms] = useState("");
+  const [doiss,setDoiss] = useState("");
+  const [tress,setTress] = useState("");
+  const [quatros,setQuatros] = useState("");
+  const [cincos,setCincos] = useState("");
+  const [seiss,setSeiss] = useState("");
+  const [setes,setSetes] = useState("");
+  const [oitos,setOitos] = useState("");
+  const [noves,setNoves] = useState("");
+
+  const [vez, setVez] = useState("O");
+  const MudaVez =()=>{
+    if(vez==="O"){
+      setVez("X");
+    }else{
+      setVez("O")
+    }
   }
-  const clear =()=>{
-    setCount(0);
+  const [um,setUm] = useState("");
+  const btnUm= ()=>{  
+    if(vez==="X"){
+      setUm(vez);       
+    }else{
+      setUm(vez);       
+    }
+    setUms(true);
+    MudaVez();
+  }  
+  const [dois,setDois] = useState("");
+  const btnDois= ()=>{
+    if(vez==="X"){
+      setDois(vez);
+       
+    }else{
+      setDois(vez);
+       
+    }
+    setDoiss(true);
+    MudaVez();
   }
-  return (
-    <View style={styles.Container}>
+  const [tres,setTres] = useState("");
+  const btnTres= ()=>{
+    if(vez==="X"){
+      setTres(vez);
+       
+    }else{
+      setTres(vez);
+       
+    }
+    setTress(true);
+    MudaVez();
+  }
+  /*linha 2*/
+  const [quatro,setQuatro] = useState("");
+  const btnQuatro= ()=>{
+    if(vez==="X"){
+      setQuatro(vez);
+       
+    }else{
+      setQuatro(vez);
+       
+    }
+    setQuatros(true);
+    MudaVez();    
+  }
+  const [cinco,setCinco] = useState("");
+  const btnCinco= ()=>{
+    if(vez==="X"){
+      setCinco(vez);
+       
+    }else{
+      setCinco(vez);
+       
+    }
+    setCincos(true);
+    MudaVez();
+  }
+  const [seis,setSeis] = useState("");
+  const btnSeis= ()=>{
+    if(vez==="X"){
+      setSeis(vez);
+       
+    }else{
+      setSeis(vez);
+       
+    }
+    setSeiss(true);
+    MudaVez();
+  }
+  /*linha 3*/
+  const [sete,setSete] = useState("");
+  const btnSete= ()=>{
+    if(vez==="X"){
+      setSete(vez);
+       
+    }else{
+      setSete(vez);
+       
+    }
+    setSetes(true);
+    MudaVez();
+  }
+  const [oito,setOito] = useState("");
+  const btnOito= ()=>{
+    if(vez==="X"){
+      setOito(vez);
+       
+    }else{
+      setOito(vez);
+       
+    }
+    setOitos(true);
+    MudaVez();
+  }
+  const [nove,setNove] = useState("");
+  const btnNove= ()=>{    
+    if(vez==="X"){
+      setNove(vez);
+       
+    }else{
+      setNove(vez);
+       
+    }
+    setNoves(true);    
+    MudaVez(); 
+  }
+  const habilitaButtons= ()=>{
+    setUms(false);
+    setDoiss(false);
+    setTress(false);
+    setQuatros(false);
+    setCincos(false);
+    setSeiss(false);
+    setSetes(false);
+    setOitos(false);
+    setNoves(false);
+  }
+  const desabilitaButtons= ()=>{
+    setUms(true);
+    setDoiss(true);
+    setTress(true);
+    setQuatros(true);
+    setCincos(true);
+    setSeiss(true);
+    setSetes(true);
+    setOitos(true);
+    setNoves(true);
+  }
+  const limpaButtons= ()=>{
+    setUm("");
+    setDois("");
+    setTres("");
+    setQuatro("");
+    setCinco("");
+    setSeis("");
+    setSete("");
+    setOito("");
+    setNove("");
+  }
+  useEffect(() => {
+    if(um!="" && dois!="" && tres!=""&& um==dois && um==tres){desabilitaButtons();Alert.alert("Vitoria de: "+um); }
+    if(um!="" && quatro!="" && sete!=""&& um==quatro && um==sete){desabilitaButtons();Alert.alert("Vitoria de: "+um); }
+    if(um!="" && cinco!="" && nove!=""&& um==cinco && um==nove){desabilitaButtons();Alert.alert("Vitoria de: "+um); }
+    if(tres!="" && cinco!="" && sete!=""&& tres==cinco && tres==sete){desabilitaButtons();Alert.alert("Vitoria de: "+tres); }
+    if(tres!="" && seis!="" && nove!=""&& tres==seis && tres==nove){desabilitaButtons();Alert.alert("Vitoria de: "+tres); }
+    if(dois!="" && cinco!="" && oito!=""&& dois==cinco && dois==oito){desabilitaButtons();Alert.alert("Vitoria de: "+dois); }
+    if(quatro!="" && cinco!="" && seis!=""&& quatro==cinco && quatro==seis){desabilitaButtons();Alert.alert("Vitoria de: "+quatro); }
+    if(sete!="" && oito!="" && nove!=""&& sete==oito && sete==nove){desabilitaButtons();Alert.alert("Vitoria de: "+sete); }
+  }, [um,dois,tres, quatro,cinco,seis,sete,oito,nove]);
+
+  const reiniciar= ()=>{
+    habilitaButtons();
+    limpaButtons();
+    Alert.alert('Jogo reiniciado');
+  }
+  
+  return (  
+    <>
+    <View style={styles.tudo}>
       <View style={styles.Count}>
-        <Text>Numero: {count}</Text>
+        <Text>Vez de: {vez}</Text>
       </View>
-    <TouchableOpacity style={styles.Button} onPress={Increment}>
-      <Text style={styles.TextButton}>Incrementar</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.Button} onPress={clear}>
-      <Text style={styles.TextButton}>Zerar</Text>
-    </TouchableOpacity>
-    </View>
+      <View style={styles.titulo}>
+        <Text style={{color:"red",fontSize:50,fontWeight: "bold"}}>Tic</Text>
+        <Text style={{fontSize:50}}>-</Text>
+        <Text style={{color:"green",fontSize:50,fontWeight: "bold"}}>Tac</Text>
+        <Text style={{fontSize:50}}>-</Text>
+        <Text style={{color:"yellow",fontSize:50,fontWeight: "bold"}}>Toe</Text>
+      </View>
+        <View style={styles.linhaUm}>
+        <TouchableOpacity style={styles.Button} disabled={ums} onPress={btnUm}>
+          <Text style={styles.TextButton}>{um}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={doiss} onPress={btnDois}>
+          <Text style={styles.TextButton}>{dois}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={tress} onPress={btnTres}>
+          <Text style={styles.TextButton}>{tres}</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.linhaDois}>
+        <TouchableOpacity style={styles.Button} disabled={quatros} onPress={btnQuatro}>
+          <Text style={styles.TextButton}>{quatro}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={cincos} onPress={btnCinco}>
+          <Text style={styles.TextButton}>{cinco}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={seiss} onPress={btnSeis}>
+          <Text style={styles.TextButton}>{seis}</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.linhaTres}>
+        <TouchableOpacity style={styles.Button} disabled={setes} onPress={btnSete}>
+          <Text style={styles.TextButton}>{sete}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={oitos} onPress={btnOito}>
+          <Text style={styles.TextButton}>{oito}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button} disabled={noves} onPress={btnNove}>
+          <Text style={styles.TextButton}>{nove}</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.Count}>
+          <Button
+            onPress={reiniciar}
+            title="Reiniciar Jogo"
+            color="#841584"
+          />
+        </View>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  Container:{
+  tudo:{
+    flex:1,
+    backgroundColor:"#e0e0eb"
+  },
+  titulo:{
+    fontWeight: "bold",
+    flexDirection: 'row',
+    justifyContent:"center",
+    paddingHorizontal:10,
+    marginVertical:50,
+    paddingHorizontal:10,
+    fontSize:25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 6,
+  },
+  Reiniciar:{
     flex:1,
     justifyContent:"center",
-    paddingHorizontal:10
+    paddingHorizontal:10,
+  },
+  linhaUm:{   
+    flexDirection: 'row',
+    justifyContent:"center",
+    paddingHorizontal:10,
+    marginVertical:5
+  },
+  linhaDois:{ 
+    flexDirection: 'row',
+    justifyContent:"center",
+    paddingHorizontal:10,
+    marginVertical:5
+  },
+  linhaTres:{
+    flexDirection: 'row',
+    justifyContent:"center",
+    paddingHorizontal:10,
+    marginVertical:5 
   },
   TextButton:{
-    color:"white"
+    flex:1,
+    fontSize:25,
+    color:"white",
+    fontWeight: "bold"
   },
   Button: {
-    alignItems: "center",
-    backgroundColor: "blue",
-    padding: 10    
+    width: 100,
+    height: 100,
+    backgroundColor:'#00bfff',
+    padding: 25,
+    marginHorizontal:5,
+    alignItems:"center" 
   },
   Count:{
-    fontSize:50,
+    fontSize:100,
     color:"blue",
     padding:10
   }
